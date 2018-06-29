adf47a77268a474c60666320aea87fef17fc51b3
Justin Knueppel <justinknueppel@gmail.com>
2018-06-29 14:35:57 -0400

    Split script into functions for readability

--- HelpWithReminderCalls.py
+++ HelpWithReminderCalls.py
@@ -6,47 +6,73 @@ from selenium.webdriver.common.keys import Keys
 
 url = 'https://secure.acuityscheduling.com/login.php'
 
-username = login.login.username
-password = login.login.password
-
 browser = webdriver.Firefox()
 browser.get(url)
-#Log in
-user_slot = browser.find_element_by_css_selector('input.input-email')
-user_slot.send_keys(username)
 
-pass_slot = browser.find_element_by_css_selector('input.input-password')
-pass_slot.send_keys(password)
-
-login_button = browser.find_element_by_css_selector('input.input-login')
-login_button.click()
-
-#Get a patient's box
+#Used CSS
 appointment_button_class = 'appointment'
 edit_note_button_css = '.edit-appointment-notes a.btn'
 save_button_css = "input[value='Save Changes']"
 
-for elem in browser.find_elements_by_class_name(appointment_button_class):
-    elem.click()
-
-    #Get patient's phone number
+def copy_phone_num():
     phone_num = browser.find_element_by_css_selector('span a.real-link').get_attribute('href')[4:]
-    #pyperclip.copy(phone_num)
+    pyperclip.copy(phone_num)
     print('Phone number: {} copied to clipboard'.format(phone_num))
 
-    #Edit the note
-    #input('Enter the type of call: ')
+def click_edit_button():
     edit_button = browser.find_element_by_css_selector(edit_note_button_css)
     actions = webdriver.ActionChains(browser)
     actions.move_to_element(edit_button)
     actions.click(edit_button)
     actions.perform()
 
-    note_text_area = browser.find_element_by_id('appt-notes')
-    note_text_area.send_keys(Keys.CONTROL + Keys.END)
-    note_text_area.send_keys(Keys.RETURN, Keys.RETURN, 'hi')
+def login():
+    user_slot = browser.find_element_by_css_selector('input.input-email')
+    user_slot.send_keys(login.login.username)
 
+    pass_slot = browser.find_element_by_css_selector('input.input-password')
+    pass_slot.send_keys(login.login.password)
+
+    login_button = browser.find_element_by_css_selector('input.input-login')
+    login_button.click()
+
+def click_save_button():
     browser.find_element_by_css_selector(save_button_css).click()
 
+def get_custom_note():
+    return ""
+    
+def edit_note_text():
+    note_text_area = browser.find_element_by_id('appt-notes')
+    note_text_area.send_keys(Keys.CONTROL + Keys.END)
+    note_text_area.send_keys(Keys.RETURN, Keys.RETURN)
+    note = get_custom_note()
+    note_text_area.send_keys(note)
+
+def return_to_appointments():
     browser.find_element_by_css_selector('a[href="/appointments.php"]').click()
+
+#Log in
+login()
+
+#Get a patient's box
+
+for elem in browser.find_elements_by_class_name(appointment_button_class):
+    elem.click()
+
+    #Copy patient's phone number
+    copy_phone_num()
+
+    #Get a custom reminder message for the client
+
+    #input('Enter the type of call: ')
+
+    #Edit the note
+    click_edit_button()
+
+    edit_note_text()
+
+    click_save_button()
+
+    return_to_appointments()
     break
