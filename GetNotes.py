b45a61fd49d0795f3984dd4cf9c0624843994370
Justin Knueppel <justinknueppel@gmail.com>
2018-07-18 18:11:21 -0400

    Have GetNotes parse the json instead of txt

--- GetNotes.py
+++ GetNotes.py
@@ -1,22 +1,16 @@
+import pyperclip, json
 
-import pyperclip
+#Load the json file
+jsonfile = open('people.json')
+people = json.load(jsonfile)
+jsonfile.close()
 
-with open("calls.txt", "r") as infile:
-    lines = list(map(lambda x : x.rstrip(), infile.readlines()))
-    index = 0
-    while index < len(lines):
-        
-        if lines[index].isdigit():
-            phonenum = lines[index]
-            input('Press enter to get phone num')
-            pyperclip.copy(phonenum)
+#Get information for each person
+for person in people:
+    phonenum = person['phone_num']
+    input('Press enter to get phone num')
+    pyperclip.copy(phonenum)
 
-            message = lines[index + 1]
-            input('Press enter to get message')
-            pyperclip.copy(message)
-
-            note = lines[index + 2]
-
-            index += 3
-        else:
-            index += 1
+    message = person['message']
+    input('Press enter to get message')
+    pyperclip.copy(message)
